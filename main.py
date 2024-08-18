89eca9f49666647d4905012ffefea019b270c5bb
Faruk Hossain <hossainf114@gmail.com>
2024-08-18 19:15:03 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+from fastapi import FastAPI, UploadFile, File, HTTPException, Request
+import multiprocessing
+from starlette.middleware.sessions import SessionMiddleware
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from utils import (
+    process_pdf, 
+    get_text_chunks, 
+    get_conversation_chain, 
+    get_vectorstore, 
+    generate_file_path,
+    save_vectorstore,
+    load_vectorstore,
+)
+
+app = FastAPI()
+load_dotenv()
+app.add_middleware(SessionMiddleware, secret_key=os.environ.get('SECRET_KEY'))
+
+
+@app.post("/upload-pdf/")
+async def read_pdfs(request: Request, files: list[UploadFile] = File(...)):
+    ip_address = request.client.host
+    try:
+        file_contents = [await file.read() for file in files]
+
+        # Use multiprocessing to handle the PDF processing in parallel
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+            results = pool.map(process_pdf, file_contents)
+
+        # Combine all extracted text into a single variable
+        combined_text = " ".join(results)
+
+        # get the text chunks
+        text_chunks = get_text_chunks(combined_text)
+
+        # create vector store
+        vectorstore = get_vectorstore(text_chunks)
+
+        # Genarate file path
+        file_path = generate_file_path(ip_address=ip_address)
+
+        # Save vectorstore in file
+        save_vectorstore(vectorstore=vectorstore, file_path=file_path)
+
+        return {"message": "PDF processed and vector store created."}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+class QuestionRequest(BaseModel):
+    question: str
+
+@app.post("/ask-question/")
+async def ask_question(request: Request, body: QuestionRequest ):
+    ip_address = request.client.host
+    vectorstore_path = generate_file_path(ip_address)
+    # Load vectorstore
+    vectorstore = load_vectorstore(vectorstore_path)
+
+    if not vectorstore:
+        vectorstore_path = generate_file_path('default')
+        vectorstore = load_vectorstore(vectorstore_path)
+        # return {"message": "No vectorstore found for the IP address"}
+
+    # Retrieve conversation history from session
+    session = request.session
+    conversation_history = session.get("conversation_history", [])
+
+    # Append the new question to conversation history
+    conversation_history.append({"role": "user", "content": body.question})
+
+    response = get_conversation_chain(vectorstore, body.question )
+    answer = response['answer']
+
+    # Update conversation history with the response
+    conversation_history.append({"role": "assistant", "content": answer})
+
+    # Save updated conversation history to session
+    session["conversation_history"] = conversation_history
+
+    return {
+        "answer": answer,
+        "conversation": conversation_history,
+    }
---
+++ utils.py
@@ -0,0 +1,110 @@
+import io
+import os
+import pickle
+import hashlib
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+def generate_file_path(ip_address: str) -> str:
+    directory = os.path.join(os.getcwd(),'pkls')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return os.path.join(directory, f'vectorstore_{ip_address}.pkl')
+
+def save_vectorstore(vectorstore, file_path):
+    with open(file_path, 'wb') as f:
+        pickle.dump(vectorstore, f)
+
+def load_vectorstore(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as f:
+            return pickle.load(f)
+    return None
+
+def calculate_data_hash(data: str) -> str:
+    return hashlib.sha256(data.encode()).hexdigest()
+
+def process_pdf(file_content: bytes) -> str:
+    try:
+        pdf_file = io.BytesIO(file_content)
+        reader = PdfReader(pdf_file)
+
+        # Extract text from all pages
+        text = ""
+        for page in reader.pages:
+            text +=  page.extract_text()
+
+        return text
+    except Exception as e:
+        raise Exception(f"Error processing PDF: {str(e)}")
+    
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    # Remove newline characters from each chunk
+    cleaned_chunks = [chunk.replace('\n', ' ').strip() for chunk in chunks]
+
+    return cleaned_chunks
+
+def get_vectorstore(text_chunks):
+    try:
+        hf_embeddings = HuggingFaceInferenceAPIEmbeddings(
+                    api_key=os.environ.get('HUGGINGFACEHUB_API_TOKEN'),
+                    model_name="sentence-transformers/all-MiniLM-l6-v2"
+                )
+        
+        # Create a vector store with FAISS using the embeddings
+        vectorstore = FAISS.from_texts(texts=text_chunks, embedding=hf_embeddings)
+        return vectorstore
+    except Exception as e:
+        raise Exception(f"Error while embeding: {str(e)}")
+    
+def get_conversation_chain(vectorstore, question):
+    try:
+        retriever = vectorstore.as_retriever()
+        repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+
+        llm = HuggingFaceEndpoint(
+            repo_id=repo_id,
+            max_length=128,
+            temperature=0.5,
+            huggingfacehub_api_token=os.environ.get('HUGGINGFACEHUB_API_TOKEN'),
+        )
+
+        # 2. Incorporate the retriever into a question-answering chain.
+        system_prompt = (
+            "You are an assistant for question-answering tasks. "
+            "Use the following pieces of retrieved context to answer "
+            "the question. If you don't know the answer, say that you "
+            "don't know. Use three sentences maximum and keep the "
+            "answer concise."
+            "\n\n"
+            "{context}"
+        )
+
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{input}"),
+            ]
+        )
+        question_answer_chain = create_stuff_documents_chain(llm, prompt)
+        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        response = rag_chain.invoke({"input": question})
+        return response
+    except Exception as e:
+        raise Exception(f"Error while embeding: {str(e)}")
+    
